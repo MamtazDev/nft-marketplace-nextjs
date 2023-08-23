7cf387a1a47503f61f7e4b24c147bfd9798e4b2c
mohi14 <mohiuddinkhan1409@gmail.com>
2023-08-23 16:34:40 +0600

    bootstarp issue solve

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
